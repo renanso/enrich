efa359a8153c45ded72d1d973308bbe27ac002ab
rsouza <renansagro@gmail.com>
2025-01-06 21:47:39 -0600

    Update slide_window.R
    replaced "length" by "nchar" because of an error on using larger input genomes.

--- R/slide_window.R
+++ R/slide_window.R
@@ -8,7 +8,7 @@ slide_window <- function(sequence, window_size, slide_size)
 {
   chars <- strsplit(sequence, "")[[1]]
   windows <- list()
-  for (i in seq(from=1, to=(length(chars) - window_size + 1), by = slide_size)) {
+  for (i in seq(from=1, to=(nchar(chars) - window_size + 1), by = slide_size)) {
     windows[[i]] <- paste(chars[i:(i + window_size - 1)], collapse = "")
   }
   return(windows)
